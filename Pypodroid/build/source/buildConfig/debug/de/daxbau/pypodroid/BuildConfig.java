71bb2bf9394a6d8db2212489cbb4812bd2579f2f
Jens Kadenbach <kadenbach@eventit.local>
2013-10-14 10:33:07 +0200

    Initial commit

---
+++ Pypodroid/build/source/buildConfig/debug/de/daxbau/pypodroid/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.daxbau.pypodroid;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
